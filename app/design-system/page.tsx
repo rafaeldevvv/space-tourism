1c27a6ab05aa9279cabbc812fc6e6aa52a732653
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2023-11-15 20:11:03 -0300

    Fix: Fix import statements in design system

--- app/design-system/page.tsx
+++ app/design-system/page.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import styles from "../index.module.css";
+import utilityClasses from "../shared-css/utility-classes.module.css";
+import componentsStyles from "../shared-css/components.module.css";
 import classes from "../utils/classes";
 
 type CSSProps = React.CSSProperties;
 
 export default function DesignSystemPage() {
   return (
-    <div className={styles.container} style={{ marginBottom: "50vh" }}>
+    <div className={utilityClasses.container} style={{ marginBottom: "50vh" }}>
       <h1
-        className={classes(styles.textWhite, styles.uppercase)}
+        className={classes(utilityClasses.textWhite, utilityClasses.uppercase)}
         style={{ marginBottom: "3rem" }}
       >
         Design System
@@ -17,30 +18,30 @@ export default function DesignSystemPage() {
       <section
         id="colors"
         style={{ marginBottom: "3rem" }}
-        className={styles.flow}
+        className={utilityClasses.flow}
       >
-        <h2 className={styles.numberedTitle}>
+        <h2 className={utilityClasses.numberedTitle}>
           <span>01</span> COLORS
         </h2>
 
-        <div className={styles.flex}>
+        <div className={utilityClasses.flex}>
           <ColorBox
             hex="#0B0D17"
             hsl="230, 35%, 7%"
             rgb="11, 13, 23"
-            className={classes(styles.bgDark, styles.textWhite)}
+            className={classes(utilityClasses.bgDark, utilityClasses.textWhite)}
           />
           <ColorBox
             hex="#0B0D17"
             hsl="231, 77%, 90%"
             rgb="208, 214, 249"
-            className={classes(styles.bgLight, styles.textDark)}
+            className={classes(utilityClasses.bgLight, utilityClasses.textDark)}
           />
           <ColorBox
             hex="#FFFFFF"
             hsl="0, 0%, 100%"
             rgb="255, 255, 255"
-            className={classes(styles.bgWhite, styles.textDark)}
+            className={classes(utilityClasses.bgWhite, utilityClasses.textDark)}
           />
         </div>
       </section>
@@ -48,13 +49,13 @@ export default function DesignSystemPage() {
       <section
         id="typography"
         style={{ margin: "4rem 0" }}
-        className={styles.flow}
+        className={utilityClasses.flow}
       >
-        <h2 className={styles.numberedTitle}>
+        <h2 className={utilityClasses.numberedTitle}>
           <span>02 </span>
           Typography
         </h2>
-        <div className={styles.flex}>
+        <div className={utilityClasses.flex}>
           <div
             style={
               {
@@ -62,76 +63,76 @@ export default function DesignSystemPage() {
                 "--flow-space": "3rem",
               } as CSSProps
             }
-            className={styles.flow}
+            className={utilityClasses.flow}
           >
             <div>
-              <p className={classes(styles.textLight)}>
+              <p className={classes(utilityClasses.textLight)}>
                 Heading 1 - Bellefair Regular - 150px
               </p>
               <p
                 className={classes(
-                  styles.fs900,
-                  styles.ffSerif,
-                  styles.uppercase
+                  utilityClasses.fs900,
+                  utilityClasses.ffSerif,
+                  utilityClasses.uppercase
                 )}
               >
                 Earth
               </p>
             </div>
             <div>
-              <p className={classes(styles.textLight)}>
+              <p className={classes(utilityClasses.textLight)}>
                 Heading 2 - Bellefair Regular - 100px
               </p>
               <p
                 className={classes(
-                  styles.fs800,
-                  styles.ffSerif,
-                  styles.uppercase
+                  utilityClasses.fs800,
+                  utilityClasses.ffSerif,
+                  utilityClasses.uppercase
                 )}
               >
                 Venus
               </p>
             </div>
             <div>
-              <p className={classes(styles.textLight)}>
+              <p className={classes(utilityClasses.textLight)}>
                 Heading 3 - Bellefair Regular - 56px
               </p>
               <p
                 className={classes(
-                  styles.fs700,
-                  styles.ffSerif,
-                  styles.uppercase
+                  utilityClasses.fs700,
+                  utilityClasses.ffSerif,
+                  utilityClasses.uppercase
                 )}
               >
                 Jupiter & Saturn
               </p>
             </div>
             <div>
-              <p className={classes(styles.textLight)}>
+              <p className={classes(utilityClasses.textLight)}>
                 Heading 4 - Bellefair Regular - 32px
               </p>
               <p
                 className={classes(
-                  styles.fs600,
-                  styles.ffSerif,
-                  styles.uppercase
+                  utilityClasses.fs600,
+                  utilityClasses.ffSerif,
+                  utilityClasses.uppercase
                 )}
               >
                 Uranus, Neptune, & Pluto
               </p>
             </div>
             <div>
-              <p className={classes(styles.textLight)}>
+              <p className={classes(utilityClasses.textLight)}>
                 Heading 5 - Barlow Condensed Regular - 28px - 4.75 Character
                 Space
               </p>
               <p
                 className={classes(
-                  styles.fs500,
-                  styles.uppercase,
-                  styles.ffSansCond,
-                  styles.letterSpacing1,
-                  styles.textLight
+                  utilityClasses.fs500,
+                  utilityClasses.uppercase,
+                  utilityClasses.ffSansCond,
+                  utilityClasses.letterSpacing1,
+                  utilityClasses.textLight
                 )}
               >
                 So, you want to travel to space
@@ -146,55 +147,55 @@ export default function DesignSystemPage() {
                 "--flow-space": "3rem",
               } as CSSProps
             }
-            className={styles.flow}
+            className={utilityClasses.flow}
           >
             <div>
-              <p className={classes(styles.textLight)}>
+              <p className={classes(utilityClasses.textLight)}>
                 Subheading 1 - Bellefair Regular - 28px
               </p>
               <p
                 className={classes(
-                  styles.uppercase,
-                  styles.fs500,
-                  styles.ffSerif
+                  utilityClasses.uppercase,
+                  utilityClasses.fs500,
+                  utilityClasses.ffSerif
                 )}
               >
                 384,400 km
               </p>
             </div>
             <div>
-              <p className={classes(styles.textLight)}>
+              <p className={classes(utilityClasses.textLight)}>
                 Subheading 2 - Barlow Condensed Regular - 14px - 2.35 Character
                 Space
               </p>
               <p
                 className={classes(
-                  styles.fs200,
-                  styles.uppercase,
-                  styles.ffSansCond,
-                  styles.letterSpacing3
+                  utilityClasses.fs200,
+                  utilityClasses.uppercase,
+                  utilityClasses.ffSansCond,
+                  utilityClasses.letterSpacing3
                 )}
               >
                 Avg. Distance
               </p>
             </div>
             <div>
-              <p className={classes(styles.textLight)}>
+              <p className={classes(utilityClasses.textLight)}>
                 Nav Text - Barlow Condensed Regular - 16px - 2.7 Character Space
               </p>
               <p
                 className={classes(
-                  styles.fs300,
-                  styles.uppercase,
-                  styles.ffSansCond,
-                  styles.letterSpacing2
+                  utilityClasses.fs300,
+                  utilityClasses.uppercase,
+                  utilityClasses.ffSansCond,
+                  utilityClasses.letterSpacing2
                 )}
               >
                 Europa
               </p>
             </div>
             <div>
-              <p className={classes(styles.textLight)}>Body Text</p>
+              <p className={classes(utilityClasses.textLight)}>Body Text</p>
               <p>
                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
                 Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi
@@ -216,8 +217,8 @@ export default function DesignSystemPage() {
         </div>
       </section>
 
-      <section className={styles.flow} id="interactive-elements">
-        <h2 className={styles.numberedTitle}>
+      <section className={utilityClasses.flow} id="interactive-elements">
+        <h2 className={utilityClasses.numberedTitle}>
           <span>03</span> Interactive Elements
         </h2>
 
@@ -226,19 +227,19 @@ export default function DesignSystemPage() {
           <nav>
             <ul
               className={classes(
-                styles.flex,
-                styles.primaryNavigation,
-                styles.underlineIndicators
+                utilityClasses.flex,
+                utilityClasses.primaryNavigation,
+                componentsStyles.underlineIndicators
               )}
             >
-              <li className={styles.active}>
+              <li className={utilityClasses.active}>
                 <a
                   href="#"
                   className={classes(
-                    styles.uppercase,
-                    styles.textLight,
-                    styles.fs300,
-                    styles.letterSpacing2
+                    utilityClasses.uppercase,
+                    utilityClasses.textLight,
+                    utilityClasses.fs300,
+                    utilityClasses.letterSpacing2
                   )}
                 >
                   <span>00</span> Active
@@ -248,10 +249,10 @@ export default function DesignSystemPage() {
                 <a
                   href="#"
                   className={classes(
-                    styles.uppercase,
-                    styles.textLight,
-                    styles.fs300,
-                    styles.letterSpacing2
+                    utilityClasses.uppercase,
+                    utilityClasses.textLight,
+                    utilityClasses.fs300,
+                    utilityClasses.letterSpacing2
                   )}
                 >
                   <span>01</span> Hovered
@@ -261,10 +262,10 @@ export default function DesignSystemPage() {
                 <a
                   href="#"
                   className={classes(
-                    styles.uppercase,
-                    styles.textLight,
-                    styles.fs300,
-                    styles.letterSpacing2
+                    utilityClasses.uppercase,
+                    utilityClasses.textLight,
+                    utilityClasses.fs300,
+                    utilityClasses.letterSpacing2
                   )}
                 >
                   <span>02</span> Idle
@@ -274,19 +275,19 @@ export default function DesignSystemPage() {
           </nav>
         </div>
 
-        <div className={styles.flex} style={{ "--gap": "3rem" } as CSSProps}>
+        <div className={utilityClasses.flex} style={{ "--gap": "3rem" } as CSSProps}>
           <div style={{ marginTop: "5rem" }}>
             {/* <!-- explore button --> */}
             <a
               href="#"
               className={classes(
-                styles.largeButton,
-                styles.uppercase,
-                styles.fs600,
-                styles.letterSpacing2,
-                styles.bgWhite,
-                styles.textDark,
-                styles.ffSerif
+                componentsStyles.largeButton,
+                utilityClasses.uppercase,
+                utilityClasses.fs600,
+                utilityClasses.letterSpacing2,
+                utilityClasses.bgWhite,
+                utilityClasses.textDark,
+                utilityClasses.ffSerif
               )}
             >
               EXPLORE
@@ -294,23 +295,23 @@ export default function DesignSystemPage() {
           </div>
 
           <div
-            className={styles.flow}
+            className={utilityClasses.flow}
             style={{ "--flow-space": "4rem" } as CSSProps}
           >
             {/* <!-- Tabs --> */}
             <div
               className={classes(
-                styles.flex,
-                styles.underlineIndicators,
-                styles.tabList
+                utilityClasses.flex,
+                componentsStyles.underlineIndicators,
+                utilityClasses.tabList
               )}
             >
               <button
                 className={classes(
-                  styles.uppercase,
-                  styles.textLight,
-                  styles.letterSpacing2,
-                  styles.ffSansCond
+                  utilityClasses.uppercase,
+                  utilityClasses.textLight,
+                  utilityClasses.letterSpacing2,
+                  utilityClasses.ffSansCond
                 )}
                 aria-selected="true"
               >
@@ -318,10 +319,10 @@ export default function DesignSystemPage() {
               </button>
               <button
                 className={classes(
-                  styles.uppercase,
-                  styles.textLight,
-                  styles.letterSpacing2,
-                  styles.ffSansCond
+                  utilityClasses.uppercase,
+                  utilityClasses.textLight,
+                  utilityClasses.letterSpacing2,
+                  utilityClasses.ffSansCond
                 )}
                 aria-selected="false"
               >
@@ -329,10 +330,10 @@ export default function DesignSystemPage() {
               </button>
               <button
                 className={classes(
-                  styles.uppercase,
-                  styles.textLight,
-                  styles.letterSpacing2,
-                  styles.ffSansCond
+                  utilityClasses.uppercase,
+                  utilityClasses.textLight,
+                  utilityClasses.letterSpacing2,
+                  utilityClasses.ffSansCond
                 )}
                 aria-selected="false"
               >
@@ -342,29 +343,29 @@ export default function DesignSystemPage() {
 
             {/* <!-- Dots --> */}
             <div
-              className={classes(styles.flex, styles.dotIndicators)}
+              className={classes(utilityClasses.flex, componentsStyles.dotIndicators)}
               style={{ "--gap": "1rem" } as CSSProps}
             >
               <button aria-selected="true">
-                <span className={styles.srOnly}>Slide title</span>
+                <span className={utilityClasses.srOnly}>Slide title</span>
               </button>
               <button aria-selected="false">
-                <span className={styles.srOnly}>Slide title</span>
+                <span className={utilityClasses.srOnly}>Slide title</span>
               </button>
               <button aria-selected="false">
-                <span className={styles.srOnly}>Slide title</span>
+                <span className={utilityClasses.srOnly}>Slide title</span>
               </button>
             </div>
 
             {/* <!-- Numbers --> */}
-            <div className={classes(styles.numberIndicators, styles.flow)}>
+            <div className={classes(componentsStyles.numberIndicators, utilityClasses.flow)}>
               <button
                 aria-selected="true"
                 className={classes(
-                  styles.ffSerif,
-                  styles.bgDark,
-                  styles.textWhite,
-                  styles.fs500
+                  utilityClasses.ffSerif,
+                  utilityClasses.bgDark,
+                  utilityClasses.textWhite,
+                  utilityClasses.fs500
                 )}
               >
                 1
@@ -372,10 +373,10 @@ export default function DesignSystemPage() {
               <button
                 aria-selected="false"
                 className={classes(
-                  styles.ffSerif,
-                  styles.bgDark,
-                  styles.textWhite,
-                  styles.fs500
+                  utilityClasses.ffSerif,
+                  utilityClasses.bgDark,
+                  utilityClasses.textWhite,
+                  utilityClasses.fs500
                 )}
               >
                 2
@@ -383,10 +384,10 @@ export default function DesignSystemPage() {
               <button
                 aria-selected="false"
                 className={classes(
-                  styles.ffSerif,
-                  styles.bgDark,
-                  styles.textWhite,
-                  styles.fs500
+                  utilityClasses.ffSerif,
+                  utilityClasses.bgDark,
+                  utilityClasses.textWhite,
+                  utilityClasses.fs500
                 )}
               >
                 3
@@ -413,28 +414,28 @@ function ColorBox({
   return (
     <div
       style={{ flexGrow: "1", "--flow-space": ".5em" } as CSSProps}
-      className={styles.flow}
+      className={utilityClasses.flow}
     >
       <div
         style={{
           padding: "3rem 1rem 1rem",
           border: "1px solid white",
         }}
-        className={className + " " + classes(styles.ffSerif, styles.fs500)}
+        className={className + " " + classes(utilityClasses.ffSerif, utilityClasses.fs500)}
       >
         {hex}
       </div>
       <p>
-        <span className={styles.textLight} style={{ marginInlineEnd: "3rem" }}>
+        <span className={utilityClasses.textLight} style={{ marginInlineEnd: "3rem" }}>
           RGB
         </span>{" "}
-        <span className={styles.textWhite}>{rgb}</span>
+        <span className={utilityClasses.textWhite}>{rgb}</span>
       </p>
       <p>
-        <span className={styles.textLight} style={{ marginInlineEnd: "3rem" }}>
+        <span className={utilityClasses.textLight} style={{ marginInlineEnd: "3rem" }}>
           HSL
         </span>{" "}
-        <span className={styles.textWhite}>{hsl}</span>
+        <span className={utilityClasses.textWhite}>{hsl}</span>
       </p>
     </div>
   );
